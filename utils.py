2c5b9f72462cda99719fc5c3686e8eb9cf2201f6
TylerDurham <TylerDurham@users.noreply.github.com>
2024-07-15 08:52:15 -0500

    Removed mockaroo Added location Typo

--- utils.py
+++ utils.py
@@ -8,21 +8,17 @@ load_dotenv()
 
 API_KEY = os.environ["TOMORROW_API_KEY"]
 USE_MOCK = True if (os.environ["USE_MOCK"] == "1") else False
-MOCK_API_KEY = os.environ["MOCKAROO_API_KEY"]
 
-def fetch_weather():
+def fetch_weather(location='chicago', units='imperial'):
     headers = { "accept": "application/json" }
     if USE_MOCK == True:
-        print("Using mock API")
-        headers = {
-            "accept": "application/json",
-            "X-API-Key": MOCK_API_KEY
-        }
-        url = f'https://my.api.mockaroo.com/tomorrow.io.json'
+        print("Using mock API. Make sure Live Server Visual Studio Code extension [https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer] is running!")
+        url = f'http://localhost:5500/sample.json'
     else:
         print("Using real API")
-        url = f'https://api.tomorrow.io/v4/weather/realtime?location=chicago&apikey={API_KEY}'
-        
+        url = f'https://api.tomorrow.io/v4/weather/realtime?location={location}&units={units}&apikey={API_KEY}'
+    
+    print(f'Calling [${url}]')    
     return requests.get(url, headers=headers)
 
 item = "org.freedesktop.Notifications"
@@ -31,7 +27,7 @@ notfy_intf = dbus.Interface(
     dbus.SessionBus().get_object(item, "/"+item.replace(".", "/")), item)
 
 def notify(title, message, icon, timeout=3000):
-
+    print(title)
     notfy_intf.Notify(
         "", 
         0, 
@@ -48,6 +44,6 @@ def get_icon(code, time):
     isNight = 0 if (time.hour % 12 == 0) else 1
         
     pattern = f'{os.path.join(os.getcwd(), 'images')}/{code}{isNight}_*@2x.png'
-    print(pattern)
+    # print(pattern)
     files = glob(pattern)
     return (files[0])
--- weather-toast.py
+++ weather-toast.py
@@ -2,8 +2,18 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 import json
 import utils
+import argparse
 
-response = utils.fetch_weather()
+parser = argparse.ArgumentParser(
+                    prog='Weather Toast',
+                    description='Toasts the current weather.',
+                    epilog='More info and source code at https://github.com/TylerDurham/weather-toast')
+
+parser.add_argument('-l', '--location')
+
+args = parser.parse_args()
+
+response = utils.fetch_weather(args.location)
 
 weather = json.loads(response.text)["data"]
 
@@ -11,10 +21,13 @@ c_time = datetime.strptime(weather["time"], "%Y-%m-%dT%H:%M:%S%f%z").astimezone(
 # TODO: Figure out if tomorrow.io suppports imperial units or if I need to do the converstions manually.
 c_temp = weather["values"]["temperature"]
 c_code = weather["values"]["weatherCode"]
+# c_location = weather["location"]["name"]
+
+print (weather)
 
 # TODO: Need to figure out why some weatherCodes returned from Mockeroo don't have icons associated with them
 c_icon = utils.get_icon(c_code, c_time)
 
-message = f'The current temperatur is {c_temp}'
+message = f'The current temperature at is {c_temp}'
 
 utils.notify("Weather Toast!", message, c_icon)
